404f96fe37761931dcf80fd7890976aef2280e6f
SHEJIN08 <mohammedshejin08@gmail.com>
2026-01-17 14:07:12 +0530

    test7...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/' , (req,res) => {
-    res.json({status: 200, message: 'Hello world'})
+    res.json({status: 200, message: 'Hello Shejin'})
 })
 
 app.listen(4000, () => {
